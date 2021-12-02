eb7c998fcd3680e43d0b66ffd9012fd715f611f8
katrinesando <kmsa@itu.dk>
2021-12-02 09:59:56 +0100

    Added film.txt og serier.txt Added method to Stream class to read through .txt

--- src/Streaming.java
+++ src/Streaming.java
@@ -1,5 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Streaming {
+    //felter
+    ArrayList<String[]> arr;
+    String str;
+    String[] line;
+    Medie medie;
+    public Streaming() {
+        //inistatitere felter
+        arr = new ArrayList<>();
+        line = null;
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\film.txt"))) //åbner fil og begynder at læse igennem
+        {
+            //kører hele fil igennem indtil der ikke er mere og tilføjer dem til array
+            while ((str = br.readLine()) != null) {
+                line = str.trim().split(";");
+                medie = new Movie(line[0],line[1],line[2],line[3]);
+                //arr.add((str.trim().split(";")));
+            }
+            System.out.println(Arrays.toString(arr.get(0))); //eksempel med at tage første element ud
+            System.out.println("Genre: "+medie.getYear());
+            System.out.println("Genre: "+medie.getGenre());
+            //tager og iterator igennem hele array'et
+           /* for (String[] s : arr) {
+                String lineStr = Arrays.toString(s);
+                System.out.println(lineStr);
+            }*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hej");
+        new Streaming();
+
     }
 }

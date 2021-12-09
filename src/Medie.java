b01ed32075b75f707f5874f44599a60bf2c82fa7
Smus4 <raoo@itu.dk>
2021-12-09 10:51:41 +0100

    12

--- src/Medie.java
+++ src/Medie.java
@@ -1,12 +1,9 @@
-import java.awt.image.BufferedImage;
-
 public abstract class Medie {
     String title, year, genre, rating;
     public abstract String getGenre();
     public abstract String getTitle();
     public abstract String getYear();
     public abstract String getRating();
-    public abstract BufferedImage getImage(String title);
 
     public Medie(String title, String year, String genre, String rating){
         this.title = title;
--- src/Streaming.java
+++ src/Streaming.java
@@ -1,8 +1,11 @@
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Streaming {
     //felter
@@ -11,27 +14,45 @@ public class Streaming {
     private static String str;
     private static Medie medie;
 
-    //loader filer for Movie og Serie
+    //loader poster
+    private static BufferedImage getImage(String title) {
+        BufferedImage img;
+        //new File();
+        // This time, you can use an InputStream to load
+        try
+        {
+            // læser fra fil og gemmer i img
+            img = ImageIO.read(new File("src\\filmplakater\\" + title+".jpg"));
+            // Then read it.
+            //java.awt.Desktop.getDesktop().open(new File("src\\filmplakater\\" + title+".jpg"));
+            return img;
+        } catch (IOException e) {
+            System.out.println("The image was not loaded.");
+            //System.exit(1);
+        }
+        return null;
+    }
+    //loader filer
     private static void loadFile() {
+        //inistatitere felter
         arr = new ArrayList<>();
-        try{
-            //læser txt for film med BufferedReader og gemmer i Array
-            BufferedReader br1 = new BufferedReader(new FileReader("src\\film.txt"));
-            while ((str = br1.readLine())!=null){
-                String[] line = str.trim().split(";");
+        String[] line;
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\film.txt"))) //åbner fil og begynder at læse igennem
+        {
+            //kører hele fil igennem indtil der ikke er mere og tilføjer dem til array
+            while ((str = br.readLine()) != null) {
+                line = str.trim().split(";");
                 medie = new Movie(line[0],line[1],line[2],line[3]);
                 arr.add(medie);
             }
 
-            //læser txt for serier med BufferedReader og gemmer i Array
-            BufferedReader br2 = new BufferedReader(new FileReader("src\\serier.txt"));
-            while ((str = br2.readLine()) != null) {
-                String[] line = str.trim().split(";");
-                medie = new Serie(line[0], line[1], line[2], line[3], line[4]);
-                arr.add(medie);
-            }
+            System.out.println("title: "+medie.getTitle());
+            System.out.println("year: "+medie.getYear());
+            System.out.println("Genre: "+medie.getGenre());
+            System.out.println("rating: "+medie.getRating());
+            //tager og iterator igennem hele array'et
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -40,12 +61,11 @@ public class Streaming {
         poster = new ArrayList<>();
         loadFile();
         for(Medie m : arr){
-            poster.add(m.getImage(m.getTitle()));
+            poster.add(getImage(medie.getTitle()));
         }
         System.out.println(poster.size());
     }
     public static void main(String[] args) {
-        loadFile();
         poster();
     }
 }

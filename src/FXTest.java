d549196d547d86d84ddb81db2617dbc61078bec7
katrinesando <kmsa@itu.dk>
2021-12-09 11:45:02 +0100

    fixed FX

--- src/FXTest.java
+++ src/FXTest.java
@@ -13,12 +13,11 @@ public class FXTest extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(FXTest.class.getResource("FxTest.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(FXTest.class.getResource("FXViewTest.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Sup");
 
         stage.setScene(scene);
         stage.show();
-
     }
 }

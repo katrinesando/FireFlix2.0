25000bf43a0e64e1200ddd92a51057609e167f94
katrinesando <kmsa@itu.dk>
2021-12-09 16:27:44 +0100

    Tried to get FX to work

--- src/FXTest.java
+++ src/FXTest.java
@@ -1,22 +1,32 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import javax.swing.*;
+import javax.swing.text.Element;
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FXTest extends Application {
 
     public static void main(String[] args) {
+
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(FXTest.class.getResource("FXViewTest.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Sup");
-
         stage.setScene(scene);
         stage.show();
     }

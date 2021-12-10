5d032884b6931d3b9894dcd5aeda41ed395b4615
katrinesando <kmsa@itu.dk>
2021-12-10 11:58:26 +0100

    FX works yay

--- src/Streaming.java
+++ src/Streaming.java
@@ -5,47 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Streaming {
-    //felter
-    private static ArrayList<Medie> arr;
-    private static ArrayList<BufferedImage> poster;
-    private static String str;
-    private static Medie medie;
 
-    //loader filer for Movie og Serie
-    private static void loadFile() {
-        arr = new ArrayList<>();
-        try{
-            //læser txt for film med BufferedReader og gemmer i Array
-            BufferedReader br1 = new BufferedReader(new FileReader("src\\film.txt"));
-            while ((str = br1.readLine())!=null){
-                String[] line = str.trim().split(";");
-                medie = new Movie(line[0],line[1],line[2],line[3]);
-                arr.add(medie);
-            }
-
-            //læser txt for serier med BufferedReader og gemmer i Array
-            BufferedReader br2 = new BufferedReader(new FileReader("src\\serier.txt"));
-            while ((str = br2.readLine()) != null) {
-                String[] line = str.trim().split(";");
-                medie = new Serie(line[0], line[1], line[2], line[3], line[4]);
-                arr.add(medie);
-            }
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-    //kobler alle film til poster og gemmer i array
-    private static void poster(){
-        poster = new ArrayList<>();
-        loadFile();
-        for(Medie m : arr){
-            poster.add(m.getImage(m.getTitle()));
-        }
-        System.out.println(poster.size());
-    }
     public static void main(String[] args) {
-        loadFile();
-        poster();
+
     }
 }
